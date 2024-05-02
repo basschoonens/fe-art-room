37a28219afa9ac754596e100a504f6ff7d98ed7c
basschoonens <bas.schoonens@gmail.com>
2024-05-02 14:19:19 +0200

    navigation bar verder uitgewerkt, links aangemaakt en icons geimporteerd. Icons nog updaten met beter geschikten

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,6 +1,8 @@
 import './Navigation.css';
 import logo from '../../assets/logo-the-art-room-no-background.png';
 import {NavLink} from "react-router-dom";
+import shoppingCart from '../../assets/shopping_cart_FILL0_wght400_GRAD0_opsz24.svg';
+import profile from '../../assets/person_FILL0_wght400_GRAD0_opsz24.svg';
 
 
 export default function Navigation() {
@@ -10,19 +12,26 @@ export default function Navigation() {
                 <span className="navbar-logo-img">
                     <img src={logo} alt="The Art Room Logo"/>
                 </span>
-                <span className="navbar-title"><span/>The Art Room</span>
                 <ul className="nav-links">
                     <li>
                         <NavLink className={({isActive}) => isActive ? "active-link" : "default-link"}
-                                 to="/">Home</NavLink>
+                                 to="/">For artlovers</NavLink>
                     </li>
                     <li>
-                        <NavLink className={({isActive}) => isActive ? "active-link" : "default-link"} to="/newpost">New
-                            Post</NavLink>
+                        <NavLink className={({isActive}) => isActive ? "active-link" : "default-link"} to="/newpost">For artists</NavLink>
                     </li>
                     <li>
                         <NavLink className={({isActive}) => isActive ? "active-link" : "default-link"}
-                                 to="/overview">Overview</NavLink>
+                                 to="/overview">About the gallery</NavLink>
+                    </li>
+                </ul>
+                {/*TODO Deze icons nog in kleur aanpassen*/}
+                <ul className="icon-links">
+                    <li>
+                        <img src={shoppingCart} alt="Shopping Cart"/>
+                    </li>
+                    <li>
+                        <img src={profile} alt="Profile"/>
                     </li>
                 </ul>
             </div>
