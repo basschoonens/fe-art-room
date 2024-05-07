5573171ddd0cdb85ad4b651c35cad57bcfe68a02
basschoonens <bas.schoonens@gmail.com>
2024-05-07 05:40:04 +0200

    about pagina uitgewerkt. Links nog toevoegen en button bijwerken.

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,7 +3,7 @@ import styles from './Button.module.css';
 
 export default function Button({ id, type, onClick, text, className }) {
     // Concatenate the original button class name with the provided className
-    const buttonClassName = `${styles.button}.${className}`;
+    const buttonClassName = `${styles}.${className}`;
 
     return (
         <button className={buttonClassName} id={id} type={type} onClick={onClick}>
