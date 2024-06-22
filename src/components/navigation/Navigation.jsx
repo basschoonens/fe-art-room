548502c7d29551d2137709aa8d1abfea9f1247d8
basschoonens <bas.schoonens@gmail.com>
2024-06-22 22:08:36 +0200

    hamburger menu gemaakt.

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,47 +1,52 @@
-import styles from './Navigation.module.css';
-import Logo from '../logo/Logo.jsx';
-import { NavLink, useNavigate} from "react-router-dom";
+import React, { useState, useContext } from 'react';
+import { NavLink, useNavigate } from "react-router-dom";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import {AuthContext} from "../../context/AuthContext.jsx";
-import {useContext} from "react";
+import { AuthContext } from "../../context/AuthContext.jsx";
+import Logo from '../logo/Logo.jsx';
+import styles from './Navigation.module.css';
+import hamburgerStyles from "../hamburgerMenu/HamburgerMenu.module.css";
 
-export default function Navigation() {
-
+const Navigation = () => {
     const navigate = useNavigate();
-    const {isAuth, user} = useContext(AuthContext);
+    const { isAuth, user } = useContext(AuthContext);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
         <nav>
             <div className={styles.navbar}>
-                <Logo/>
-                <ul className={styles.navLinks}>
+                <Logo />
+                <ul className={`${styles.navLinks} ${isMenuOpen ? styles.navOpen : ''}`}>
                     <li>
-                        <NavLink className={({isActive}) => isActive ? styles.activeLink : styles.defaultLink}
+                        <NavLink className={({ isActive }) => isActive ? styles.activeLink : styles.defaultLink}
                                  to="/maingallery">For artlovers</NavLink>
                     </li>
                     {isAuth && (user.authority === "ROLE_ARTIST" || user.authority === "ROLE_ADMIN") && (
                         <li>
-                            <NavLink className={({isActive}) => isActive ? styles.activeLink : styles.defaultLink}
+                            <NavLink className={({ isActive }) => isActive ? styles.activeLink : styles.defaultLink}
                                      to="/artistgallery">For artists</NavLink>
                         </li>
                     )}
                     {isAuth && user.authority === "ROLE_ADMIN" && (
                         <li>
-                            <NavLink className={({isActive}) => isActive ? styles.activeLink : styles.defaultLink}
+                            <NavLink className={({ isActive }) => isActive ? styles.activeLink : styles.defaultLink}
                                      to="/admin">For admin</NavLink>
                         </li>
                     )}
                     <li>
-                        <NavLink className={({isActive}) => isActive ? styles.activeLink : styles.defaultLink}
+                        <NavLink className={({ isActive }) => isActive ? styles.activeLink : styles.defaultLink}
                                  to="/about">About the gallery</NavLink>
                     </li>
                 </ul>
                 <ul className={styles.iconLinks}>
                     <li>
                         {isAuth ? (
-                            <FaShoppingCart onClick={() => navigate("/shoppingcart")} className={styles.icon}/>
+                            <FaShoppingCart onClick={() => navigate("/shoppingcart")} className={styles.icon} />
                         ) : (
-                            <FaShoppingCart onClick={() => navigate("/login")} className={styles.icon}/>
+                            <FaShoppingCart onClick={() => navigate("/login")} className={styles.icon} />
                         )}
                     </li>
                     <li>
@@ -52,7 +57,14 @@ export default function Navigation() {
                         )}
                     </li>
                 </ul>
+                <div className={`${hamburgerStyles.menuIcon} ${isMenuOpen ? hamburgerStyles.open : ''}`} onClick={toggleMenu}>
+                    <div className={hamburgerStyles.bar}></div>
+                    <div className={hamburgerStyles.bar}></div>
+                    <div className={hamburgerStyles.bar}></div>
+                </div>
             </div>
         </nav>
     );
-}
+};
+
+export default Navigation;
